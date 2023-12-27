2da88299555e0c029d269341f3484d3d97cdda8a
necromicom <ebrancatti@gmail.com>
2023-12-27 00:59:57 -0300

    actualizo proyecto

--- preentrega/aplicacion/urls.py
+++ preentrega/aplicacion/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from aplicacion.views import leer_autor,
+from aplicacion.views import leer_autor
 urlpatterns = [
     path("autor/", leer_autor),
 ]
--- preentrega/aplicacion/views.py
+++ preentrega/aplicacion/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from aplicacion.models import Autor, 
+from aplicacion.models import Autor
 
 
 
 def leer_autor(request):
-    texto = Autor()
     return render(request, "autor.html")
